72f74ddd8a804102d9d038d2cf5c9cf566938189
LukhaC2tsh <chulumancolt@gmail.com>
2025-09-26 20:11:42 +0200

    modified html script element

---
+++ scripts/modal.js
@@ -0,0 +1,12 @@
+function openModal(){
+    document.getElementById('task-modal').showModal();
+}
+
+function closeModal(){
+    document.getElementById('task-modal').close();
+}
+
+
+function initializeModal(){
+    
+}
