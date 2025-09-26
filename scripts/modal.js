79291b182516d7fb369c08c249c00853156c5124
LukhaC2tsh <chulumancolt@gmail.com>
2025-09-26 22:58:49 +0200

    adds initializeModal function: added call to setupDropdown function, setupDropdown function

--- scripts/modal.js
+++ scripts/modal.js
@@ -7,6 +7,49 @@ function closeModal(){
 }
 
 
-function initializeModal(){
+export function initializeModal(task){
+    document.getElementById("task-title").value = task.title;
+    document.getElementById("task-desc").value = task.description;
+    document.getElementById("task-status").innerContent = task.status;
+
+    setupDropdown(task);
+    openModal();
+}
+
+
+
+function setupDropdown(task){
+    const optionsContainer = document.getElementById('status-option-container');
+    const optionsTrigger = document.getElementById('task-status-event');
+    const listItemTrigger = optionsContainer.querySelectorAll('.status-options li');
+
+    //sets the color of the options 
+    listItemTrigger.forEach(li => {
+      li.style.color = "var(--secondary-font-color)";
+      li.style.fontWeight = "500";
+
+      li.addEventListener("click", function(){
+        document.getElementById("task-status").innerText = li.innerText.trim();
+        optionsContainer.classList.remove('show-options');
+        return li.innerText.trim();
+      }
+        
+    )})
+
+    
+
+    optionsTrigger.querySelector('#task-status').innerText = task.status;
+
+    optionsTrigger.addEventListener("click", function(event){
+        event.stopPropagation();
+        optionsContainer.classList.toggle('show-options');
+    })
+    
+
+    document.addEventListener("click", function(event) {
+        if (!optionsContainer.contains(event.target) && !optionsTrigger.contains(event.target)) {
+        optionsContainer.classList.remove('show-options');
+        }
+    });
     
 }
